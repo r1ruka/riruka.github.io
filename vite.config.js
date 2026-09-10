613b8dbd39b6140033ed8fb55932ec51d3be635d
r1ruka <0xd1v45@gmail.com>
2026-09-10 22:05:42 +0530

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/riruka.github.io/',
   plugins: [react()],
   assetsInclude: ['**/*.mp3'],
 })
